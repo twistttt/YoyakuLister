07cc81d562e2ada31ff3179920246550bbfe8f4e
twistttt <72329083+twistttt@users.noreply.github.com>
2022-03-09 12:09:06 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,108 @@
+// obs-websocket-js by Brendan Hagan
+// Copyright (c) 2016 Brendan Hagan
+// Licensed under MIT (https://github.com/obs-websocket-community-projects/obs-websocket-js/blob/master/LICENSE.md)
+
+// youtube-chat by LinaTsukusu
+// Copyright (c) 2021 LinaTsukusu
+// Licensed under MIT (https://github.com/LinaTsukusu/youtube-chat/blob/develop/LICENSE)
+
+const { LiveChat } = require("youtube-chat")
+
+const obs = new OBSWebSocket();
+
+var liveChat, yoyakuword, keshiword, chid, list, eibox, yoyakuretsu;
+var address, pass, sourcename;
+var is_connected = false;
+var sankasya = new Array();
+list = document.getElementById('list');
+eibox = document.getElementById('enable_input');
+
+function yoyakustart() {
+    yoyakuword = document.getElementById('yoyakuword').value;
+    keshiword = document.getElementById('keshiword').value;
+    chid = document.getElementById('chid').value;
+    liveChat = new LiveChat({ channelId: chid });
+    liveChat.start();
+
+    liveChat.on("chat", (chatItem) => {
+        if (chatItem.message[0].text === yoyakuword) {
+            sankasya.push(chatItem.author.name);
+            yoyakuretsu = sankasya.toString().replace(/,/g, "\n");
+            list.value = yoyakuretsu;
+            if (is_connected === true) {
+                obs.send('SetTextFreetype2Properties', {
+                    'source': sourcename,
+                    'text': yoyakuretsu
+                })
+            }
+        }
+        if (chatItem.message[0].text === keshiword) {
+            sankasya.splice(sankasya.indexOf(chatItem.author.name), 1);
+            yoyakuretsu = sankasya.toString().replace(/,/g, "\n");
+            list.value = yoyakuretsu;
+            if (is_connected === true) {
+                obs.send('SetTextFreetype2Properties', {
+                    'source': sourcename,
+                    'text': yoyakuretsu
+                })
+            }
+        }
+    })
+
+    liveChat.on("start", (liveId) => {
+        document.getElementById('status').textContent = "状態：稼働中";
+    })
+
+    liveChat.on("end", (reason) => {
+        document.getElementById('status').textContent = "状態：停止中";
+    })
+
+    liveChat.on("error", (err) => {
+        document.getElementById('status').textContent = "状態：エラー";
+    })
+}
+
+function yoyakustop() {
+    liveChat.stop();
+}
+
+eibox.addEventListener('change', function() {
+
+    if (eibox.checked == true) {
+        list.readOnly = false;
+    } else {
+        list.readOnly = true;
+        yoyakuretsu = list.value;
+        sankasya = yoyakuretsu.split('\n')
+        if (is_connected == true) {
+            obs.send('SetTextFreetype2Properties', {
+                'source': sourcename,
+                'text': yoyakuretsu
+            })
+        }
+    }
+}, false)
+
+function obsstart() {
+    address = document.getElementById('address').value;
+    pass = document.getElementById('pass').value;
+    sourcename = document.getElementById('sourcename').value;
+    obs.connect({
+            address: 'localhost:' + address,
+            password: pass
+        })
+        .then(() => {
+            is_connected = true;
+            document.getElementById('statobs').textContent = "状態：接続中";
+        })
+
+    obs.on('error', err => {
+        document.getElementById('statobs').textContent = "状態：エラー";
+    });
+}
+
+function obsstop() {
+    obs.disconnect()
+    is_connected = false;
+    document.getElementById('statobs').textContent = "状態：接続解除中";
+}
---
+++ main.js
@@ -0,0 +1,67 @@
+// YoyakuLister
+// Copyright © 2022 Twist
+// Licensed under MIT (https://opensource.org/licenses/mit-license.php)
+
+const { app, Menu, BrowserWindow } = require('electron');
+
+const isMac = (process.platform === 'darwin');
+let mainWindow;
+
+const menu = Menu.buildFromTemplate([
+    ...(isMac ? [{
+        label: app.name,
+        submenu: [
+            { role: 'about', label: `${app.name}について` },
+            { type: 'separator' },
+            { role: 'services', label: 'サービス' },
+            { type: 'separator' },
+            { role: 'hide', label: `${app.name}を隠す` },
+            { role: 'hideothers', label: 'ほかを隠す' },
+            { role: 'unhide', label: 'すべて表示' },
+            { type: 'separator' },
+            { role: 'quit', label: `${app.name}を終了` }
+        ]
+    }] : []),
+    {
+        label: 'ファイル',
+        submenu: [
+            isMac ? { role: 'quit', label: '終了' } : { role: 'quit', label: '終了' }
+        ]
+    },
+    {
+        label: "編集",
+        submenu: [
+            { label: "カット", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "コピー", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "ペースト", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "すべて選択", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]
+    }
+])
+
+Menu.setApplicationMenu(menu);
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        width: 650,
+        height: 700,
+        resizable: false,
+        fullscreenable: false,
+    });
+
+    mainWindow.loadFile('index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
